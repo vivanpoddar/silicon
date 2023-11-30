a02c0668b22f4cf6bb9ffb03f5d30c259a9f28a3
Vivan Poddar <vivanneil@outlook.com>
2023-11-30 10:30:32 -0500

    new build

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,29 +3,29 @@ import Image from "next/image"
 
 const Footer = (): JSX.Element => {
     return (
-        <footer className="bg-white shadow dark:bg-gray-900">
+        <footer className="bg-gray-800 shadow">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <Link href="/" className="flex items-center w-auto mb-4 sm:mb-0 space-x-2 rtl:space-x-reverse">
+                    <Link href="/" className="flex items-center w-auto mb-4 space-x-2 rtl:space-x-reverse">
                         <img src="/image.png" className="h-10" alt="Silicon" />
                     </Link>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium">
                         <li>
-                            <Link href="#" className="hover:underline me-4 md:me-6">About</Link>
+                            <Link href="#" className="hover:underline me-4 md:me-6 text-white">About</Link>
                         </li>
                         <li>
-                            <Link href="#" className="hover:underline me-4 md:me-6">Join</Link>
+                            <Link href="#" className="hover:underline me-4 md:me-6 text-white">Join</Link>
                         </li>
                         <li>
-                            <Link href="#" className="hover:underline me-4 md:me-6">Management</Link>
+                            <Link href="#" className="hover:underline me-4 md:me-6 text-white">Management</Link>
                         </li>
                         <li>
-                            <Link href="#" className="hover:underline">Contact</Link>
+                            <Link href="#" className="hover:underline text-white">Contact</Link>
                         </li>
                     </ul>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="joinsilicon.org" className="hover:underline">Silicon Foundation</Link><br></br>Website by Vivan Poddar</span>
+                <hr className="my-6 text-white border-gray-700 lg:my-8" />
+                <span className="block text-sm text-white">© 2023 <Link href="joinsilicon.org" className="hover:underline">Silicon Foundation</Link><br></br>Website by Vivan Poddar</span>
             </div>
         </footer>
     )
--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <>
             <Navbar current="About"/>
-            <div className="h-screen bg-whiteflex items-center justify-center">
+            <div className="h-screen bg-white flex items-center justify-center">
                 <h1 className="text-7xl text-black">Compute. Connect. Innovate.</h1>
             </div>
             <Footer />
