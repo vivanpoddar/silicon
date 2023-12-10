9bf59f3395d9852b2fa91d4ef4b287111c15c1e5
Vivan Poddar <vivanneil@outlook.com>
2023-12-10 12:41:00 -0500

    new build

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -17,7 +17,7 @@ const Person = ({ image, position, text, name, linkedin, email }: PersonProps) =
     return (
         <div className="w-auto h-auto border rounded-lg overflow-hidden p-4">
             <div className="w-32 h-24 overflow-hidden">
-                <Image unoptimized src={image} width={640} height={480} alt={name} layout="responsive" />
+                <Image src={image} width={640} height={480} alt={name} layout="responsive" />
             </div>
             <h1 className="text-3xl text-white">{name}</h1>
             <h2 className="text-xl text-gray-400">{position}</h2>
@@ -47,12 +47,12 @@ const Join = () => {
                         <h1 className="text-2xl">Board of Directors</h1>
                         <div className="border-b w-full mb-2"></div>
                         <div className="flex space-x-4">
-                            <Person image="vivan.png" name="Vivan Poddar" position="Executive Director" text="" linkedin="https://www.linkedin.com/in/vivanpoddar" email="mailto:vivanneil@outlook.com" />
-                            <Person image="evan.png" name="Evan Xiang" position="Operations Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
-                            <Person image="ethan.png" name="Ethan Chen" position="Finance, PR Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
-                            <Person image="thomas.png" name="Thomas Wang" position="Marketing Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
-                            <Person image="kiran.png" name="Kiran Tevar" position="Expansion Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
-                            <Person image="liam.png" name="Ziyu (Liam) Lin" position="Product Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/vivan.png" name="Vivan Poddar" position="Executive Director" text="" linkedin="https://www.linkedin.com/in/vivanpoddar" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/evan.png" name="Evan Xiang" position="Operations Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/ethan.png" name="Ethan Chen" position="Finance, PR Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/thomas.png" name="Thomas Wang" position="Marketing Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/kiran.png" name="Kiran Tevar" position="Expansion Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
+                            <Person image="/liam.png" name="Ziyu (Liam) Lin" position="Product Director" text="" linkedin="https://www.linkedin.com/" email="mailto:vivanneil@outlook.com" />
                         </div>
                         <h1 className="text-2xl pt-4">Regional Directors</h1>
                         <div className="border-b w-full mb-2"></div>
