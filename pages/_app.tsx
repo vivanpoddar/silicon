65726aa93767cba591e9fe9b6db31e093d8cf760
Vivan Poddar <vivanneil@outlook.com>
2023-12-11 00:20:30 -0500

    new build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,17 @@ import Image from 'next/image';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight)
 
-      const handleResize = () => setWindowWidth(window.innerWidth);
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+        setWindowHeight(window.innerHeight);
+      }
 
       window.addEventListener('resize', handleResize);
       return () => window.removeEventListener('resize', handleResize);
@@ -19,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
 
-  if (windowWidth && windowWidth < 1250) {
+  if (windowWidth && windowWidth < 1250 && windowHeight && windowHeight < 1000) {
     return (
       <div className='w-[100vw] h-[100vh] text-xl flex flex-col justify-center items-center'>
         <Image priority={true} src="/image.png" alt="Silicon" width={50} height={50} />
