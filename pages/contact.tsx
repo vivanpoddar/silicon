57c012506ed33366a9b0dcdf1663c5d821fd9d1f
Vivan Poddar <vivanneil@outlook.com>
2024-01-07 13:10:51 -0500

    new build

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,11 +13,11 @@ const Join = () => {
             <div className="overflow-hidden">
                 <div className="flex bg-black justify-center">
                     <div className="pt-24 pb-24 px-4 flex-col justify-center">
-                        <h1 className="text-2xl text-white">Board of Directors</h1>
+                        <h1 className="text-2xl text-white">Leadership</h1>
                         <div className="border-b w-full mb-2"></div>
                         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
                             <Person image="/vivan.png" name="Vivan Poddar" position="Executive Director" text="" linkedin="https://www.linkedin.com/in/vivanpoddar" email="mailto:vivanneil@outlook.com" />
-                            <Person image="/evan.png" name="Evan Xiang" position="Operations Director" text="" linkedin="https://www.linkedin.com/in/evan-xiang-4483b7231/" email="mailto:evankxiang@gmail.com" />
+                            <Person image="/evan.png" name="Evan Xiang" position="SSA.hack Director" text="" linkedin="https://www.linkedin.com/in/evan-xiang-4483b7231/" email="mailto:evankxiang@gmail.com" />
                             <Person image="/ethan.png" name="Ethan Chen" position="Finance, PR Director" text="" linkedin="https://www.linkedin.com/in/ethan-chen-065265283/" email="" />
                             <Person image="/thomas.png" name="Thomas Wang" position="Marketing Director" text="" linkedin="https://www.linkedin.com/in/thomas-jichen-wang-a0b427268/" email="" />
                             <Person image="/kiran.png" name="Kiran Tevar" position="Expansion Director" text="" linkedin="https://www.linkedin.com/in/kiran-tevar-680304278/" email="" />
