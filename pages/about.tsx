f64e7b77a250148368ca41f7e88e669770baea8a
Vivan Poddar <vivanneil@outlook.com>
2024-03-09 09:54:26 -0500

    new build

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import {motion} from "framer-motion";
-import Head from "next/head";
+import { motion } from "framer-motion";
+import Head from 'next/head'
 import Image from "next/image";
 
 const About = () => {
@@ -10,10 +10,10 @@ const About = () => {
             <Head>
                 <title>Silicon | About</title>
             </Head>
-            <Navbar current="About"/>
+            <Navbar current="About" />
             <div>
                 <div className="pt-24 bg-white flex flex-col justify-center items-center">
-                    <h1 className="md:text-7xl text-3xl text-black text-center">Compute. Connect. Innovate.</h1>
+                    <h1 className="md:text-7xl text-3xl text-black text-center">Compete. Compute. Connect.</h1>
                     <motion.div
                         className="mb-4 w-3/4 h-1 bg-black mt-4"
                         initial={{ scaleX: 0 }}
@@ -30,7 +30,7 @@ const About = () => {
                                 Silicon aims to foster the next generation of thoughtful, ethical, and interdisciplinary computer science leaders and create a universal awareness of the computer science that powers the modern world. More specifically, Silicon aims to produce leaders that can think outside the box, analyze computational solutions for potential harms to society and solve them, and connect knowledge in a breadth of fields to create new and novel solutions. Silicon also aims to provide the general public with the knowledge to understand the computer science that powers the modern digital world and how it affects them physiologically, emotionally, and psychologically.
                             </p>
                             <p className="md:text-4xl">
-                               We are looking to begin our journey by hosting a hackathon in Spring of 2024, SSA.hack(). An interest form and more information will be available soon. 
+                                We are looking to begin our journey by hosting a hackathon in Spring of 2024, shady.Hacks.
                             </p>
                         </div>
                     </div>
