614e329c9bf9959d3e9f8d367127b47fa698b6e4
twang2epic <wangthomas19@outlook.com>
2024-03-17 23:48:39 -0400

    new build

--- pages/shadyhacks.tsx
+++ pages/shadyhacks.tsx
@@ -843,8 +843,8 @@ const Sponsors = (): JSX.Element => {
                         <h1 className="text-3xl ">Atom Sponsors</h1>
                         <div className="flex">
                             <Image src="/doclens_logo.jpg" alt="Doclens" height={50} width={200}></Image>
-                            <Image src="/connectedhealth.png" alt="connectedhealth" height={91} width={400}></Image>
-                            <Image src="/echo.png" alt="echo" height={200} width={400}></Image>
+                            <Image src="/connectedhealth.png" alt="connectedhealth" height={65} width={300}></Image>
+                            <Image src="/echo.png" alt="echo" height={150} width={300}></Image>
                         </div>
                     </div>
                     <div className=" p-4 m-4">
