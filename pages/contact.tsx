12d0481ab5a7b977898c19fb66d1930d4fccbecf
Vivan Poddar <vivanneil@outlook.com>
2025-02-12 22:10:34 -0500

    added boden

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -18,20 +18,18 @@ const Join = () => {
                         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
                             <Person image="/vivan.png" name="Vivan Poddar" position="Executive Director" text="" linkedin="https://www.linkedin.com/in/vivanpoddar" email="mailto:vivanneil@outlook.com" />
                             {/* <Person image="/evan.png" name="Evan Xiang" position="SSA.hack Director" text="" linkedin="https://www.linkedin.com/in/evan-xiang-4483b7231/" email="mailto:evankxiang@gmail.com" /> */}
-                            <Person image="/thomas.png" name="Thomas Wang" position="Operations Director" text="" linkedin="https://www.linkedin.com/in/thomas-jichen-wang-a0b427268/" email="" />
-                            <Person image="/suhaan.png" name="Suhaan Tammana" position="Finances Director" text="" linkedin="" email="" />
+                            <Person image="/thomas.png" name="Thomas Wang" position="Operations Director" text="" linkedin="https://www.linkedin.com/in/thomas-jichen-wang-a0b427268/" email="mailto:wangthomas19@outlook.com" />
+                            <Person image="/boden.png" name="Boden Moraski" position="Outreach Director" text="" linkedin="https://www.linkedin.com/in/boden-moraski/overlay/contact-info/" email="bodenmoraski@gmail.com" />
                             {/* <Person image="/david.png" name="David Wang" position="Product Director" text="" linkedin="" email="" /> */}
                         </div>
-                        <h1 className="text-2xl pt-4 text-white">Regional Directors</h1>
+                        {/* <h1 className="text-2xl pt-4 text-white">Regional Directors</h1>
                         <div className="border-b w-full mb-2"></div>
                         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
-                            {/* <Person image="/liam.png" name="Ziyu (Liam) Lin" position="East Asia Region" text="" linkedin="" email="" /> */}
                         </div>
                         <h1 className="text-2xl pt-4 text-white">Chapter Directors</h1>
                         <div className="border-b w-full mb-2"></div>
                         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
-                            {/* <Person image="/anita.png" name="Anita Zhu" position="Deer Lakes School District" text="" linkedin="https://www.linkedin.com/in/anita-zhu-75b332224/details/experience/" email="" /> */}
-                        </div>
+                        </div> */}
                     </div>
 
                 </div>
