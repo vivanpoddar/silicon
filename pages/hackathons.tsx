cf051eb24b0275c7cdf41a2c786d9cacac80b484
Vivan Poddar <vivanneil@outlook.com>
2025-08-11 00:38:57 -0400

    adosjg

--- pages/hackathons.tsx
+++ pages/hackathons.tsx
@@ -196,7 +196,7 @@ export default function Hackathons() {
                                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                                     </svg>
-                                    Cards
+                                    
                                 </button>
                                 <button
                                     onClick={() => setViewMode('lines')}
@@ -209,7 +209,7 @@ export default function Hackathons() {
                                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 10h16M4 14h16M4 18h16" />
                                     </svg>
-                                    Lines
+                                    
                                 </button>
                             </div>
                         </div>
